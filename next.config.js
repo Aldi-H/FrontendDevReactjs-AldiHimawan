acfaa78b61d2fe00d36aeed8a6647f18ebd29b6d
Aldi-H <aldihimawan88@gmail.com>
2023-11-15 14:35:04 +0700

    chore: config env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    RESTAURANT_BASE_URL: process.env.RESTAURANT_BASE_URL,
+    REVIEWS_BASE_URL: process.env.REVIEWS_BASE_URL,
+    X_RAPIDAPI_KEY: process.env.X_RAPIDAPI_KEY,
+    X_RAPIDAPI_HOST: process.env.X_RAPIDAPI_HOST,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
