47af2de6a4f71ea63143f269d0b9772f3f995535
Aldi-H <aldihimawan88@gmail.com>
2023-11-17 07:41:18 +0700

    chore: configure next.congfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    RESTAURANT_BASE_URL: process.env.RESTAURANT_BASE_URL,
-    REVIEWS_BASE_URL: process.env.REVIEWS_BASE_URL,
-    X_RAPIDAPI_KEY: process.env.X_RAPIDAPI_KEY,
-    X_RAPIDAPI_HOST: process.env.X_RAPIDAPI_HOST,
+    BASE_URL: process.env.BASE_URL,
   },
 };
 
